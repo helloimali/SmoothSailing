358299baded92c4f064f673d2e741e466033e21e
helloimali <toalisaeedmalik@gmail.com>
2019-03-17 11:30:15 -0400

    Gradient background, UI updates, Hamburger menu fixed

--- app/src/main/java/com/example/alimalik/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alimalik/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.alimalik.myapplication;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
