0e59ced093ecfb4af4c17c37a2eba320454aca04
helloimali <toalisaeedmalik@gmail.com>
2019-03-17 03:35:56 -0400

    Hamburger Menu and Icons

--- app/src/main/java/com/example/alimalik/smoothsailing/MainActivity.java
+++ app/src/main/java/com/example/alimalik/smoothsailing/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.alimalik.smoothsailing;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
-    Button b1;
+    Button b1, b2;
     TextView t1;
 
     @Override
@@ -18,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         b1 = (Button) findViewById(R.id.sendButton);
+        b2 = (Button) findViewById(R.id.luggaeButton);
         t1 = (TextView) findViewById(R.id.IDTEXT);
 
         BtnClick();
@@ -30,7 +29,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view){
                 t1.setText("nice");
 
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                t1.setText("nah");
             }
         });
     }
+
+
 }
